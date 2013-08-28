b945d02dae15eb5bdc8ceea712f5b434f28c1e75
Jeremy Dahl <jeremy@palebluedot.ca>
2013-08-28 12:03:57 -0700

    Added the grid and the gallery markup

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function($) {
 
+	// to make our grid pretty
+	$('.posts').masonry({
+ 	// columnWidth: 960,
+ 	itemSelector: '.post'
+	});
+
+	// for the gallery
+	$("#slides").slidesjs({
+		navigation : {
+			effect : "fade"
+		}
+
+	});
+
 });
 
 
